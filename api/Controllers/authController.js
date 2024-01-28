c9ac1c71abd735de3894296a3de7553171d19ccd
Mohamed <mohammedmohamuud.01@gmail.com>
2024-01-28 14:37:15 +0300

    Create sign in api route

--- api/Controllers/authController.js
+++ api/Controllers/authController.js
@@ -1,5 +1,7 @@
 import User from "../Models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../Utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -8,11 +10,42 @@ export const signup = async (req, res, next) => {
 
   try {
     await newUser.save();
-    res
-      .status(201)
-      .json({ Message: "User created successfully", User: newUser });
+    res.status(201).json("User created successfully");
   } catch (error) {
     res.status(400);
     next(error);
   }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email: email });
+    if (!validUser) {
+      return next(errorHandler(401, "User not found"));
+    }
+
+    const passwordMatches = await bcrypt.compare(password, validUser.password);
+
+    if (!passwordMatches) {
+      return next(errorHandler(401, "Wrong credentials"));
+    }
+
+    const token = generateToken(validUser._id, validUser.email);
+
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie("token", token, { httpOnly: true }).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const generateToken = (id, email) => {
+  const payload = { id, email };
+  const secret = process.env.JWT_SECRET;
+  return jwt.sign(payload, secret, {
+    expiresIn: "1d",
+  });
 };
--- api/Routes/authRoutes.js
+++ api/Routes/authRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { signup } from "../Controllers/authController.js";
+import { signin, signup } from "../Controllers/authController.js";
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/signin", signin);
 
 export default router;
