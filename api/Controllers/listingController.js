6f1b6065a86fefcb2c03ed3a1548cbceb2309b31
Mohamed <mohammedmohamuud.01@gmail.com>
2024-01-31 17:31:17 +0300

    Create get user listings api route

--- api/Controllers/listingController.js
+++ api/Controllers/listingController.js
@@ -1,4 +1,5 @@
 import Listing from "../Models/listingModel.js";
+import { errorHandler } from "../Utils/error.js";
 
 export const createListing = async (req, res, next) => {
   try {
@@ -6,5 +7,18 @@ export const createListing = async (req, res, next) => {
     res.status(201).json(listing);
   } catch (error) {
     next(error);
+  }
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listing = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listing);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(403, "You only view your own listings"));
   }
 };
--- api/Routes/UserRoutes.js
+++ api/Routes/UserRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { deleteUser, updateUser } from "../Controllers/userController.js";
 import { verifyUser } from "../Utils/verify.js";
+import { getUserListings } from "../Controllers/listingController.js";
 const router = express.Router();
 
 router.put("/update/:id", verifyUser, updateUser);
 router.delete("/delete/:id", verifyUser, deleteUser);
+router.get("/listings/:id", verifyUser, getUserListings);
 
 export default router;
