8f23a4ac5689de294330b95f559dabf99efe1720
Mohamed <mohammedmohamuud.01@gmail.com>
2024-02-01 11:41:26 +0300

    Create update listing api route

--- api/Routes/listingRoutes.js
+++ api/Routes/listingRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createListing,
   deleteListing,
+  updateListing,
 } from "../Controllers/listingController.js";
 import { verifyUser } from "../Utils/verify.js";
 const router = express.Router();
 
 router.post("/create", verifyUser, createListing);
 router.delete("/delete/:id", verifyUser, deleteListing);
+router.put("/update/:id", verifyUser, updateListing);
 
 export default router;
